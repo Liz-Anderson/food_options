31a959e5c31772ebbfac88f63fe7e4c1a072cf42
liz <cleanbean@gmail.com>
2025-12-03 12:56:43 -0800

    figured out appropriate data structure for meals, added filter functions to computed

--- site.js
+++ site.js
@@ -52,29 +52,27 @@ const app = Vue.createApp({
             // ],
             // meals: {
             //     dinnerIn: [
-            //     {name: 'Tacos', description: 'Our classic bean tacos with fresh veggies', selected: false},
-            //     {name: 'Rice and Tofu', description: 'Rice bowl with baked garbonzo beans, pan fried tofu in sesame oil with braggs liquid aminos and nutritional yeast, and avocado', selected: false},
-            //     {name: 'Pesto Pasta with a Green Salad', description: 'Pesto pasta sauce and a salad with spinach/lettuce, tomato, olives, green onions, croutons, and ranch or goddess dressing', selected: false},
-            //     {name: 'Pumpkin Pasta with a Kale Salad', description: 'Pumpkin pasta sauce and a kale salad with nuts, dried cranberries, and poppyseed dressing', selected: false},
-            //     {name: 'Fondue', description: 'Vegan fondue with bread, vegan sausages, potato, broccoli, and apple/pear for dipping', selected: false},
-            //     {name: 'Breaded Tofu', description: 'Baked breaded tofu with mashed potatoes', selected: false},
-            //     {name: 'Pot Pie with a Kale Salad', description: 'Seitan pot pie with potatoes, carrots, celery, and peas served with a kale salad with poppyseed dressing', selected: false}
+            //         {name: 'Tacos', description: 'Our classic bean tacos with fresh veggies', selected: false},
+            //         {name: 'Rice and Tofu', description: 'Rice bowl with baked garbonzo beans, pan fried tofu in sesame oil with braggs liquid aminos and nutritional yeast, and avocado', selected: false},
+            //         {name: 'Pesto Pasta with a Green Salad', description: 'Pesto pasta sauce and a salad with spinach/lettuce, tomato, olives, green onions, croutons, and ranch or goddess dressing', selected: false},
+            //         {name: 'Pumpkin Pasta with a Kale Salad', description: 'Pumpkin pasta sauce and a kale salad with nuts, dried cranberries, and poppyseed dressing', selected: false},
+            //         {name: 'Fondue', description: 'Vegan fondue with bread, vegan sausages, potato, broccoli, and apple/pear for dipping', selected: false},
+            //         {name: 'Breaded Tofu', description: 'Baked breaded tofu with mashed potatoes', selected: false},
+            //         {name: 'Pot Pie with a Kale Salad', description: 'Seitan pot pie with potatoes, carrots, celery, and peas served with a kale salad with poppyseed dressing', selected: false}
             //     ],
-            //     dinnerOut: {
-            //         takeout: [
-            //             {name: 'Van Hanh', description: 'Vegan Chinese', link: 'https://veganpdx.us/', selected: false},
-            //             {name: 'Maruti', description: 'Vegan/vegetarian Indian', link: 'https://maruti-restaurant.com/', selected: false},
-            //             {name: 'Dove Vivi', description: 'Cornmeal crust pizza', link:'https://www.dovevivipizza.com/', selected: false},
-            //             {name: 'Rudy\'s', description: 'Regular pizza', link: 'http://rudyspizzapdx.com/', selected: false},
-            //             {name: 'Obon', description: 'Vegan Japanese (udon/onigiri)', link: 'https://www.obonpdx.com/', selected: false},
-            //         ],
-            //         eatIn: [
-            //             {name: 'Tusk', description: 'Mediterranean (falafel/hummus/tasty cocktails)', link: 'https://tuskpdx.com/', selected: false},
-            //             {name: 'Izakaya Kichinto', description: 'Japanese (ramen/udon/sushi)', link: 'https://izakayakichinto.com/', selected: false},
-            //             {name: 'Lilla', description: 'Vegan Italian', link: 'https://www.lillapdx.com/', selected: false},
-            //             {name: 'La Buca', description: 'Italian', link: 'https://labucaitaliancafe.com/', selected: false}
-            //         ]
-            //     },
+            //     dinnerTakeout: [
+            //         {name: 'Van Hanh', description: 'Vegan Chinese', link: 'https://veganpdx.us/', selected: false},
+            //         {name: 'Maruti', description: 'Vegan/vegetarian Indian', link: 'https://maruti-restaurant.com/', selected: false},
+            //         {name: 'Dove Vivi', description: 'Cornmeal crust pizza', link:'https://www.dovevivipizza.com/', selected: false},
+            //         {name: 'Rudy\'s', description: 'Regular pizza', link: 'http://rudyspizzapdx.com/', selected: false},
+            //         {name: 'Obon', description: 'Vegan Japanese (udon/onigiri)', link: 'https://www.obonpdx.com/', selected: false},
+            //     ],
+            //     dinnerEatOut: [
+            //         {name: 'Tusk', description: 'Mediterranean (falafel/hummus/tasty cocktails)', link: 'https://tuskpdx.com/', selected: false},
+            //         {name: 'Izakaya Kichinto', description: 'Japanese (ramen/udon/sushi)', link: 'https://izakayakichinto.com/', selected: false},
+            //         {name: 'Lilla', description: 'Vegan Italian', link: 'https://www.lillapdx.com/', selected: false},
+            //         {name: 'La Buca', description: 'Italian', link: 'https://labucaitaliancafe.com/', selected: false}
+            //     ],
             //     lunchIn: [
             //         {name: 'Chickpea Tuna Salad Sandwiches', description: 'Chickpea tuna salad made with vegenaise, mustard, celery, and other things', selected: false},
             //         {name: 'Burritos', description: 'Refried bean burritos with fresh veggies', selected: false},
@@ -157,19 +155,63 @@ const app = Vue.createApp({
 
         },
         selectMeal (meal){
-            console.log("before: ", meal.selected)
+            console.log("before: ", meal.selected, meal)
             meal.selected = !meal.selected
             console.log("after: ", meal.selected)
         },
    
     },
     computed: {
-        selected: function() {
+        selectedBreakfastIn: function() {
             
             return this.meals.filter(function(item){
-                return item.selected === true
+                return (item.selected === true && item.mealType === 'breakfastIn')
             })
-        }
+        },
+
+        selectedBreakfastOut: function() {
+            
+            return this.meals.filter(function(item){
+                return (item.selected === true && item.mealType === 'breakfastOut')
+            })
+        },
+
+        selectedLunchIn: function() {
+            
+            return this.meals.filter(function(item){
+                return (item.selected === true && item.mealType === 'lunchOut')
+            })
+        },
+
+        selectedLunchOut: function() {
+            
+            return this.meals.filter(function(item){
+                return (item.selected === true && item.mealType === 'lunchOut')
+            })
+        },
+
+        selectedDinnerIn: function() {
+            
+            return this.meals.filter(function(item){
+                return (item.selected === true && item.mealType === 'dinnerIn')
+            })
+        },
+
+        selectedDinnerTakeout: function() {
+            
+            return this.meals.filter(function(item){
+                return (item.selected === true && item.mealType === 'dinnerTakeout')
+            })
+        },
+
+        selectedDinnerEatOut: function() {
+            
+            return this.meals.filter(function(item){
+                return (item.selected === true && item.mealType === 'dinnerEatOut')
+            })
+        },
+
+
     },
 
 })
